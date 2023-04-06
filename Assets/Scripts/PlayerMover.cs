6a946429b1b2403730413d5b5d2af364beb76861
Nataliia <natalia.egorova978@gmail.com>
2023-04-06 18:58:20 +0400

    Made basic on mouse click movement

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform target;
+    private Ray lastRay;
+
+    private void Update() 
     {
+        MovePlayer();    
+    }
+
+    private void MovePlayer()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToRay();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveToRay()
     {
-        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit; 
+        bool hasHit = Physics.Raycast(ray, out hit); //store information about where the raycast hit in "hit" variable
+
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
